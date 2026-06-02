2a6f48c8490302bf50152ffcabd76754cca0c291
Augusto Llopis <augustollopisar@gmail.com>
2026-06-02 01:59:50 +0100

    fix: saltar plugin de vite usando postcss para compilar tailwind v4

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,9 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
-import tailwind from '@tailwindcss/vite'; // 🔌 Motor nativo v4
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.renwaza.com',
-
-  vite: {
-    plugins: [
-      tailwind({
-        // 🎯 EL PARCHE DE NETLIFY: Esto desactiva el campo que hacía fallar a Vite/Rolldown
-        resolveOptions: { tsconfigPaths: false }
-      })
-    ],
-  },
-
   integrations: [
     sitemap()
   ],
